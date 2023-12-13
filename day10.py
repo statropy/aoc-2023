e18882a20d517fe00cef7a07f6c22d4f69215400
Erik Larson <erik@statropy.com>
2023-12-13 14:42:20 -0500

    Day 10 Python finally done

--- day10.py
+++ day10.py
@@ -7,19 +7,21 @@ from typing import Literal
 class Pos(object):
     nextpos: dict = {"N": (0, -1), "S": (0, 1), "E": (1, 0), "W": (-1, 0)}
 
-    def __init__(self, grid: "Grid", x: int, y: int, last: "Pos" = None):
+    def __init__(
+        self,
+        grid: "Grid",
+        x: int,
+        y: int,
+        last: "Pos" = None,
+        direction: Literal["N", "S", "E", "W"] = None,
+    ):
         if x < 0 or y < 0 or x >= grid.width or y >= grid.height:
             raise ValueError("Invalid index")
         self.x: int = x
         self.y: int = y
         self.grid: Grid = grid
-        # if last is not None and self == last:
-        #     logging.error("backtrack")
-        #     raise ValueError("Backtracking")
-        # if last is None:
-        #     self.last = self
-        # else:
         self.last = last
+        self.direction = direction  # the direction last went to get here
 
     def __repr__(self):
         return f"({self.x},{self.y})"
@@ -30,12 +32,12 @@ class Pos(object):
     def __eq__(self, other):
         return isinstance(other, Pos) and self.x == other.x and self.y == other.y
 
-    def _move(self, x: int, y: int) -> "Pos":
-        return Pos(self.grid, self.x + x, self.y + y, self)
+    def spawn(self, x: int, y: int, direction: Literal["N", "S", "E", "W"]) -> "Pos":
+        return Pos(self.grid, self.x + x, self.y + y, self, direction)
 
     def move(self, direction: Literal["N", "S", "E", "W"]):
         try:
-            p = self._move(*self.nextpos[direction])
+            p = self.spawn(*self.nextpos[direction], direction)
             if self.last is None or self.last != p:
                 self.grid.add(p, direction)
         except ValueError as e:
@@ -45,23 +47,17 @@ class Pos(object):
 class Grid(object):
     legal_to = {"N": "|7F", "S": "|LJ", "E": "-J7", "W": "-LF"}
     legal_from = {"N": "S|LJ", "S": "S|7F", "E": "S-LF", "W": "S-J7"}
-    # logging.debug = {
-    #     "N": [ord("|"), ord("7"), ord("F")],
-    #     "S": [ord("|"), ord("L"), ord("J")],
-    #     "E": [ord("-"), ord("J"), ord("7")],
-    #     "W": [ord("-"), ord("L"), ord("F")],
-    # }
 
     def __init__(self, lines: list[str]):
-        # self.grid = lines
         self.grid = []
         self.display = []
         self.start: Pos = None
         self.nextstep = []
         self.steps = 0
-        # self.answer = 0
-        self.left = None
-        self.right = None
+        self.pinner = set()
+        self.pending = set()
+        self.inc = None
+        self.outc = None
 
         for y, row in enumerate(lines):
             self.grid.append([c for c in row.strip()])
@@ -69,17 +65,56 @@ class Grid(object):
             if self.start is None:
                 try:
                     x = row.index("S")
-                    start = Pos(self, x, y)
-                    self.current = set([start])
-                    self.visited = set([start])
+                    self.start = Pos(self, x, y)
+                    self.current = set([self.start])
+                    self.visited = set([self.start])
                 except ValueError:
                     continue
 
-    def tile(self, pos: Pos) -> str:
+    def gridtile(self, pos: Pos) -> str:
         return self.grid[pos.y][pos.x]
+
+    def gridtileat(self, x: int, y: int) -> str:
+        return self.grid[y][x]
+
+    def tile(self, pos: Pos) -> str:
+        return self.display[pos.y][pos.x]
+
+    def tileat(self, x: int, y: int) -> str:
+        return self.display[y][x]
+
+    def clearat(self, x: int, y: int):
+        self.display[y][x] = " "
 
     def mark(self, pos: Pos, c: str = " "):
         self.display[pos.y][pos.x] = c
+
+    def markcurrent(self, pos: Pos):
+        self.mark(pos, "*")
+
+    def markouterat(self, x: int, y: int):
+        p = Pos(self, x, y)
+        self.markouter(p)
+
+    def markouter(self, pos: Pos):
+        self.mark(pos, "O")
+
+    def markinner(self, pos: Pos):
+        self.mark(pos, "I")
+        self.pinner.add(pos)
+
+    def markinnerat(self, x: int, y: int):
+        p = Pos(self, x, y)
+        self.markinner(p)
+
+    def markpath(self, pos: Pos):
+        self.mark(pos, "+")
+
+    def markright(self, pos: Pos):
+        self.mark(pos, "R")
+
+    def markleft(self, pos: Pos):
+        self.mark(pos, "Q")
 
     @property
     def width(self) -> int:
@@ -91,11 +126,10 @@ class Grid(object):
 
     def add(self, pos: Pos, direction: Literal["N", "S", "E", "W"]):
         if (
-            # pos != pos.last and
-            self.tile(pos) in self.legal_to[direction]
-            and self.tile(pos.last) in self.legal_from[direction]
+            self.gridtile(pos) in self.legal_to[direction]
+            and self.gridtile(pos.last) in self.legal_from[direction]
         ):
-            logging.debug(f" {direction} {pos.last} -> {pos} {self.tile(pos)}")
+            logging.debug(f" {direction} {pos.last} -> {pos} {self.gridtile(pos)}")
             self.nextstep.append(pos)
 
     def findloop(self):
@@ -111,29 +145,11 @@ class Grid(object):
             self.steps += 1
 
             for pos in self.current:
-                self.mark(pos, "*")
+                self.markcurrent(pos)
             if len(self.current) == 1 and len(self.nextstep) == 2:
-                logging.info(f"A: {self.steps} {self.current} {self.nextstep}")
+                logging.debug(f"A: {self.steps} {self.current} {self.nextstep}")
                 self.current = set()
-                self.mark(self.nextstep[0], "+")
-                # self.answer = self.steps
-                # logging.info(f"A: {self.steps} {self.current} {self.nextstep}")
-            #     # if self.left is not None:
-            #     #     self.mark(self.left)
-            #     found = set()
-            #     for pos in self.nextstep:
-            #         if pos in found:
-            #             self.left = pos
-            #             logging.debug(f"Remove {pos}")
-            #             self.current.remove(pos)
-            #         else:
-            #             found.add(pos)
-            #     for pos in self.nextstep:
-            #         if pos == self.left:
-            #             self.right = pos
-            #             self.mark(pos, "+")
-            # logging.info(f"A: {self.steps} {self.current} {self.nextstep}")
-            # self.answer = self.steps
+                self.markpath(self.nextstep[0])
             if logging.getLogger().level == logging.DEBUG:
                 print(self)
             for pos in self.current:
@@ -142,23 +158,133 @@ class Grid(object):
             self.mark(pos)
         return self.steps
 
+    def markside(self, pos: Pos, x, y, d, f, o):
+        try:
+            n = pos.spawn(x, y, d)
+            if self.tile(n) != "+":
+                if (d == f) != (o == "flip"):
+                    self.markright(n)
+                else:
+                    self.markleft(n)
+        except ValueError as e:
+            logging.debug(e)
+
+    def side(
+        self,
+        pos: Pos,
+        direction: Literal["N", "S", "E", "W"],
+        tile: Literal["|", "-", "L", "J", "7", "F"],
+        orientation: Literal["norm", "flip"],
+    ):
+        if tile == "|":
+            self.markside(pos, 1, 0, direction, "N", orientation)
+            self.markside(pos, -1, 0, direction, "S", orientation)
+        elif tile == "-":
+            self.markside(pos, 0, 1, direction, "E", orientation)
+            self.markside(pos, 0, -1, direction, "W", orientation)
+        elif tile == "J":
+            self.markside(pos, 1, 0, direction, "N", orientation)
+            self.markside(pos, 0, 1, direction, "N", orientation)
+            self.markside(pos, -1, -1, direction, "W", orientation)
+            pass
+        elif tile == "F":
+            self.markside(pos, 0, -1, direction, "S", orientation)
+            self.markside(pos, -1, 0, direction, "S", orientation)
+            self.markside(pos, 1, 1, direction, "E", orientation)
+            pass
+        elif tile == "L":
+            self.markside(pos, 1, -1, direction, "N", orientation)
+            self.markside(pos, 0, 1, direction, "E", orientation)
+            self.markside(pos, -1, 0, direction, "E", orientation)
+            pass
+        elif tile == "7":
+            self.markside(pos, -1, 1, direction, "S", orientation)
+            self.markside(pos, 0, -1, direction, "W", orientation)
+            self.markside(pos, 1, 0, direction, "W", orientation)
+            pass
+
+    def retrace_path(self, node, orientation):
+        while node.last is not None:
+            self.markpath(node)
+            self.side(node.last, node.direction, self.gridtile(node.last), orientation)
+            node = node.last
+
+    def count(self, c):
+        return len([c for row in self.display for col in row if col == c])
+
+    def other(self, c):
+        if c == "R":
+            return "Q"
+        return "R"
+
     def retrace(self):
-        while True:
-            for pos in self.nextstep:
-                if pos.last is None:
-                    return
-                self.mark(pos.last, "+")
-            self.nextstep = [pos.last for pos in self.nextstep]
-        # self.current = set([self.left, self.right])
+        a: Pos
+        b: Pos
+        a, b = self.nextstep
+        self.markpath(self.start)
+        self.retrace_path(a, "norm")
+        self.retrace_path(b, "flip")
 
-    def clearpipes(self):
-        # pipes = "|7FLJ-"
-        nextdisplay = []
-        for row in self.display:
-            nextdisplay.append([c if c == "+" else " " for c in row])
-        self.display = nextdisplay
+        for y, row in enumerate(self.display):
+            for x, col in enumerate(row):
+                if col not in "+QR":
+                    self.clearat(x, y)
 
-        # self.display.append([c for c in row.strip()])
+        for y, row in enumerate(self.display):
+            for x, col in enumerate(row):
+                tile = self.tileat(x, y)
+                if tile == "+":
+                    continue
+                p = Pos(self, x, y)
+                if self.inc is not None and tile in "RQ":
+                    if tile == self.inc:
+                        self.markinner(p)
+                    else:
+                        self.markouter(p)
+                elif tile in " RQ":
+                    me = self.checkNeighbors(p)
+                    if self.inc is None and tile in "RQ":
+                        if me == "O":
+                            self.outc = tile
+                            self.inc = self.other(tile)
+                        else:
+                            self.inc = tile
+                            self.outc = self.other(tile)
+                        logging.debug(f"FOUND  {me} I={self.inc} O={self.outc}")
+                    if me == "O":
+                        self.markouter(p)
+                    elif me == "I":
+                        self.markinner(p)
+        return len(self.pinner)
+
+    def checkNeighbors(self, pos: Pos):
+        dirs = ["W", "N", "E", "S"]
+        for i, (x, y) in enumerate([(-1, 0), (0, -1), (1, 0), (0, 1)]):
+            try:
+                n = pos.spawn(x, y, dirs[i])
+                t = self.tile(n)
+                tile = self.tile(pos)
+
+                if n in self.pending:
+                    continue
+
+                if t in "OIRQ":
+                    return t
+                if t == " ":
+                    if tile == "O":
+                        self.markouter(n)
+                        continue
+                    elif tile == "I":
+                        self.markinner(n)
+                        continue
+                    self.pending.add(pos)
+                    t = self.checkNeighbors(n)
+                    self.pending.remove(pos)
+                    return t
+            except ValueError:
+                return "O"
+
+        raise AssertionError
 
     def __repr__(self):
         output = ""
@@ -166,51 +292,55 @@ class Grid(object):
             output += ("".join([c for c in row])) + "\n"
         return output
 
+    def show(self):
+        output = ""
+        for row in self.grid:
+            output += ("".join([c for c in row])) + "\n"
+        return output
+
 
 def part1(lines: list[str]) -> int:
     grid = Grid(lines)
-    a = grid.findloop()
-    print(grid)
-    return a
+    return grid.findloop()
 
 
 def part2(lines: list[str]) -> int:
     grid = Grid(lines)
     grid.findloop()
-    grid.retrace()
-    grid.clearpipes()
-    print(grid)
-
-    return None
+    return grid.retrace()
 
 
 logging.basicConfig(level=logging.INFO)
 
 
 class TestDay10(unittest.TestCase):
-    # def test_1a(self):
-    #     with open("./test10.txt", "r") as f:
-    #         self.assertEqual(part1(list(f)), 4)
-
-    # def test_1b(self):
-    #     with open("./test10b.txt", "r") as f:
-    #         self.assertEqual(part1(list(f)), 8)
-
-    # def test_1(self):
-    #     with open("./input10.txt", "r") as f:
-    #         self.assertEqual(part1(list(f)), 6733)
-
-    def test_2a(self):
+    def test_1a(self):
         with open("./test10.txt", "r") as f:
-            self.assertEqual(part2(list(f)), None)
+            self.assertEqual(part1(list(f)), 4)
+
+    def test_1b(self):
+        with open("./test10b.txt", "r") as f:
+            self.assertEqual(part1(list(f)), 8)
+
+    def test_1(self):
+        with open("./input10.txt", "r") as f:
+            self.assertEqual(part1(list(f)), 6733)
 
     def test_2c(self):
         with open("./test10c.txt", "r") as f:
-            self.assertEqual(part2(list(f)), None)
+            self.assertEqual(part2(list(f)), 4)
+
+    def test_2d(self):
+        with open("./test10d.txt", "r") as f:
+            self.assertEqual(part2(list(f)), 8)
+
+    def test_2e(self):
+        with open("./test10e.txt", "r") as f:
+            self.assertEqual(part2(list(f)), 10)
 
     def test_2(self):
         with open("./input10.txt", "r") as f:
-            self.assertEqual(part2(list(f)), None)
+            self.assertEqual(part2(list(f)), 435)
 
 
 if __name__ == "__main__":
